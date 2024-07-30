58d1bf14ffca14e0f97de791be236d8125961e9c
Harut <zaqaryanharut242gmail.com>
2024-07-30 20:13:54 +0400

    Correction

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,11 +1,16 @@
-export const handler = async (event) => {
-  let path = event["path"];
+exports.handler = async (event) => {
+  console.log("event -> ", event);
+
   let method = event["httpMethod"];
+  let path = event["path"];
+
+  console.log("path -> ", path);
+  console.log("method -> ", method);
 
   if (path === "/hello") {
     return {
       statusCode: 200,
-      message: "Hello from Lambda",
+      message: "Hello from Lambda"
     };
   } else {
     return {
