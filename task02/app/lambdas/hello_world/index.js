14c4e801d255148f98b1831cb5f9344268cacef3
Harut <zaqaryanharut242gmail.com>
2024-07-31 19:02:34 +0400

    Correction

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,24 +1,18 @@
 exports.handler = async (event) => {
   console.log("event -> ", event);
 
-  let method = event["httpMethod"];
-  let path = event["path"];
+  let method = event["requestContext"]['http'].method;
+  let path = event["requestContext"]['http'].path;
 
   if (path === "/hello") {
     return {
       statusCode: 200,
-      // message: "Hello from Lambda"
       body: { message: "Hello from Lambda" }
     };
   } else {
     return {
       statusCode: 400,
-      body:{
-        message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
-      }
-      // body: JSON.stringify({
-      //   message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
-      // })
+      body:{ message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`}
     };
   }
 };
